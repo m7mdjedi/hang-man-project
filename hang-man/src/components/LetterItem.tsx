ceff7c4f09494a60d8172f6386d20f61d16b9726
Mohammed Aljedi <m7mdjedi@gmail.com>
2024-05-17 22:19:22 +0300

    version three

--- hang-man/src/components/LetterItem.tsx
+++ hang-man/src/components/LetterItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback, useEffect } from 'react'
 import {useSelector,useDispatch} from 'react-redux'
 import type { RootState } from '../store/store';
 import { addToCounter, addToSolvedWord } from '../store/wordSlice';
@@ -6,20 +6,44 @@ import { addToCounter, addToSolvedWord } from '../store/wordSlice';
 const LetterItem:React.FC<{value:string}>= (props)=> {
   const word = useSelector((state: RootState)=>state.game.word);
   const dispatch = useDispatch(); 
-  const [status , setStatus] = React.useState(true);
+  const [status , setStatus] = React.useState(0);
+  const escFunction = useCallback((event:KeyboardEvent) => {
+    if (event.keyCode === props.value.toUpperCase().charCodeAt(0) ) {
+      if(status==0){ 
+        if(word.includes(props.value)) { 
+            setStatus(1); 
+            dispatch(addToSolvedWord(props.value)); 
+        }
+        else {  
+          setStatus(2);
+          dispatch(addToCounter());
+      }
+  }
+    }
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("keydown", escFunction, false);
+
+    return () => {
+      document.removeEventListener("keydown", escFunction, false);
+    };
+  }, [escFunction]);
   const onClickHandler = ()=>{ 
-    if(status){ 
+    if(status==0){ 
           if(word.includes(props.value)) { 
-              setStatus(false); 
+              setStatus(1); 
               dispatch(addToSolvedWord(props.value)); 
           }
           else {  
+            setStatus(2); 
             dispatch(addToCounter());
         }
     }
   }
   return (
-    <div  onClick={onClickHandler} className={`w-[50px] cursor-pointer text-center rounded-md  text-3xl border-2 border-gray-500 active:bg-blue-500 ${!status?"pointer-events-none opacity-65":""}`}>
+    <div  onClick={onClickHandler} className={`w-[50px] cursor-pointer text-center  font-bold aspect-square rounded-md flex items-center justify-center  text-3xl border-2 border-gray-500  ${(status==2)?"pointer-events-none opacity-65":(status==1)? "bg-blue-500  pointer-events-none": ""}`}>
+
       {props.value}
     </div>
   )
